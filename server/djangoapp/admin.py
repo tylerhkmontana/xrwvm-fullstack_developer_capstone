73e35a124508d0b2b3cfb11f826471d35142bb6d
tylerhkmontana <tylerhkmontana@gmail.com>
2024-07-04 15:38:01 -0400

    module 3

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,12 +1,10 @@
-# from django.contrib import admin
-# from .models import related models
-
-
 # Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
-# CarModelInline class
-
-# CarModelAdmin class
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # CarMakeAdmin class with CarModelInline
 
