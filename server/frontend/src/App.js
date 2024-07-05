cf3c94b08e84b562b5fc9078d166dac755189266
tylerhkmontana <63427616+tylerhkmontana@users.noreply.github.com>
2024-07-04 20:34:57 -0400

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 8 */
+
 import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
